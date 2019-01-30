d51d5978f7afa0c26ddedb034599c6f835672c3b
James World <james-world@users.noreply.github.com>
2019-01-30 00:03:17 +0000

    Clean up dead code

--- src/index.js
+++ src/index.js
@@ -5,15 +5,6 @@ import ReactDOM from 'react-dom';
 import App from './app.js';
 
 async function start() {
-    VSS = await getVSS();
-
-    var webContext = VSS.getWebContext();
-    var projectId = webContext.project.id;
-
-    ReactDOM.render(
-        <h1>Hello { projectId }!!</h1>,
-        document.getElementById('app')
-    );
 
     ReactDOM.render(
         <App />,
